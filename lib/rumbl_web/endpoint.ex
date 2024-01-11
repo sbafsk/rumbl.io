cb8e70e6f238c03068d8f1120b159ec57a0d557b
sbafsk <razor1836@gmail.com>
2024-01-11 17:12:04 -0300

    server socket setup

--- lib/rumbl_web/endpoint.ex
+++ lib/rumbl_web/endpoint.ex
@@ -10,6 +10,10 @@ defmodule RumblWeb.Endpoint do
     signing_salt: "JMUAQMNA"
   ]
 
+  socket "/socket", RumblWeb.UserSocket,
+    websocket: true,
+    longpoll: false
+
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
