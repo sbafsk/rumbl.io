d7d8ca209a8f5b58cc4375420f4e609c6ef1653c
sbafsk <razor1836@gmail.com>
2024-01-11 17:12:27 -0300

    add annotation schema n view

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -4,11 +4,12 @@ defmodule Rumbl.Multimedia do
   """
 
   import Ecto.Query, warn: false
-  alias Rumbl.Multimedia.Category
+
   alias Rumbl.Repo
   alias Rumbl.Accounts
-
+  alias Rumbl.Multimedia.Category
   alias Rumbl.Multimedia.Video
+  alias Rumbl.Multimedia.Annotation
 
   @doc """
   Returns the list of videos.
@@ -129,5 +130,20 @@ defmodule Rumbl.Multimedia do
     Category
     |> Category.alphabetical()
     |> Repo.all()
+  end
+
+  def annotate_video(%Accounts.User{id: user_id}, video_id, attrs) do
+    %Annotation{video_id: video_id, user_id: user_id}
+    |> Annotation.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def list_annotations(%Video{} = video) do
+    Repo.all(
+      from a in Ecto.assoc(video, :annotations),
+        order_by: [asc: a.at, asc: a.id],
+        limit: 500,
+        preload: [:user]
+    )
   end
 end
