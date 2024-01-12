36d5a9003e9ffee17ed15ad28e52d03e5f6ffbb9
sbafsk <razor1836@gmail.com>
2024-01-12 12:27:53 -0300

    add presence setup

--- lib/rumbl/application.ex
+++ lib/rumbl/application.ex
@@ -14,6 +14,8 @@ defmodule Rumbl.Application do
       RumblWeb.Telemetry,
       # Start the PubSub system
       {Phoenix.PubSub, name: Rumbl.PubSub},
+      # Start the Precense tracking
+      RumblWeb.Presence,
       # Start the Endpoint (http/https)
       RumblWeb.Endpoint
       # Start a worker by calling: Rumbl.Worker.start_link(arg)
